5c5b3ec66b6cb3de9b538d90b872a38c99333150
Megh <148608551+Meghpatel2810@users.noreply.github.com>
2024-04-08 22:38:31 +0530

    Add files via upload

---
+++ Railway_ticket_booking_system.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<windows.h> //For sleep function
+using namespace std;
+
+class train{
+
+};
+
+class passenger{
+
+};
+
+//Inherit diffrent types of trains and passenger classes 
+
+int main()
+{
+    //Take necessary inputs from user
+
+    //Show a complete ticket and thank you
+}
